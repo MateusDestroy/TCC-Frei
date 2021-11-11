2e8db8a44607253d989c05341e5a33f60691d1e7
Mateus <mateusesutaquiomm@gmail.com>
2021-11-11 15:16:56 -0300

    foii

--- api/src/index.js
+++ api/src/index.js
@@ -290,11 +290,7 @@ app.post('/produto', async (req, resp) => {
 
 
 app.get('/produto', async (req, resp) => {
-
-
     const data = await db.infoa_sti_produto.findAll({where: id_categoria});
-
-    
     resp.send(data);
 
 });
